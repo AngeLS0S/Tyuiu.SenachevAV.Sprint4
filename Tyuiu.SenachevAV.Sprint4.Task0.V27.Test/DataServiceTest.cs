444ba3a8ad2fd84a022c2e1043c71a55b6605da0
Андрей Сеначев <andrey30-04-2005god@yandex.ru>
2024-11-11 01:20:20 +0500

    Task0

---
+++ Tyuiu.SenachevAV.Sprint4.Task0.V27.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+using Tyuiu.SenachevAV.Sprint4.Task0.V27.Lib;
+
+namespace Tyuiu.SenachevAV.Sprint4.Task0.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultOddArrEl()
+        {
+            DataService ds = new DataService();
+
+            int[] array = new int[] { 7, 4, 3, 2, 1, 5, 9, 8, 7, 4 };
+            int res = ds.GetMultOddArrEl(array);
+            int wait = 6615;
+
+            Assert.AreEqual(wait, res);
+        }
+
+    }
+    
+}
---
+++ Tyuiu.SenachevAV.Sprint4.Task0.V27/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.SenachevAV.Sprint4.Task0.V27.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 | Выполнил: Сеначев А. В. | АСОиУБ-24-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Одномерные массивы (статический ввод)                             *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #27                                                             *");
+        Console.WriteLine("* Выполнил: Сеначев Андрей Вячеславович  | АСОиУБ-24-1                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный         *");
+        Console.WriteLine("* статическими значениями в диапазоне от 0 до 9  подсчитать произведение  *");
+        Console.WriteLine("* нечетных элементов массива. {7 ,4 ,3 ,2 ,1 ,5 ,9 ,8 ,7 ,4}              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Исходный массив:");
+        int[] array = new int[] { 7, 4, 3, 2, 1, 5, 9, 8, 7, 4 };
+        foreach (int element in array)
+        {
+            Console.WriteLine(element);
+        }
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine($"Произведение нечётных элементов массива: {ds.GetMultOddArrEl(array)}");
+        Console.ReadKey();
+    }
+}
